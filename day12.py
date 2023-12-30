bca3058ee46ca3cf8d502eb9b897bd28b1b9242d
Rafael Lopez <lopez@airelle.info>
2023-12-30 08:13:36 -0500

    Nettoyage

--- day12.py
+++ day12.py
@@ -6,29 +6,16 @@ with open("input/input12.txt", "r") as f:
 
 @lru_cache(maxsize=None)
 def count_arrangements(data: str, code: str):
-    nb = 0
     if len(code) == 0:
-        if "#" in data or len(data) == 0:
-            return 0
-        else:
-            if len(data) > 0:
-                return 1
-            return 0
-    list_code = [int(x) for x in code.split(",")]
-    prepared = data.strip(".")
-    if len(list_code) > 0 and len(prepared) < list_code[0]:
+        if len(data) > 0 and "#" not in data:
+            return 1
         return 0
-    if len(prepared) == 0:
-        if len(code) == 0:
-            return 0
+    list_code = [int(x) for x in code.split(",")]
+    prepared = data.lstrip(".")
+    if len(prepared) == 0 or (len(list_code) > 0 and len(prepared) < list_code[0]):
         return 0
     if len(prepared) == 1:
-        if len(code) == 0 and prepared[0] == "#":
-            return 0
-        if len(code) == 0 and prepared[0] != "#":
-            print("hit b:", data, code)
-            return 1
-        if len(code) > 1:
+        if (len(code) == 0 and prepared[0] == "#") or len(code) > 1:
             return 0
     if prepared[0] == "?":
         x = count_arrangements("." + prepared[1:], code)
@@ -38,12 +25,6 @@ def count_arrangements(data: str, code: str):
         candidate = prepared[0 : list_code[0]]
         if "." in candidate:
             return 0
-        if (
-            len(list_code) == 1
-            and len(prepared) == list_code[0]
-            and "." not in prepared
-        ):
-            return 1
         if len(prepared) > list_code[0] and prepared[list_code[0]] == "#":
             return 0
         x = count_arrangements(
@@ -51,7 +32,7 @@ def count_arrangements(data: str, code: str):
             ",".join([str(x) for x in list_code[1:]]),
         )
         return x
-    return nb
+    return 0
 
 
 def unfold(spring):
